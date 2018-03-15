ebf09fed500f81a5934c7b96d7d4b71c3716c8b5
Guy <that.guy@mailhero.io>
2018-03-15 15:36:10 -0400

    Constants

--- hello.go
+++ hello.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
+const helloPrefix = "Hello, "
+
 // Hello will print out "Hello, World"
 func Hello(name string) string {
-	return "Hello, " + name
+	return helloPrefix + name
 }
 
 func main() {
